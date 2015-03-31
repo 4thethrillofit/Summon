9eea26ab74560e2b0481892d6bbcc80326461790
Feifan Wang <feifanw@gmail.com>
2015-03-30 17:43:53 -0700

    basic table setup

--- Summon/ViewController.swift
+++ Summon/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+                      UITableViewDelegate,
+                      UITableViewDataSource {
 
+    var neons: [String] = ["fei", "alex"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = 
+        let cellID = "NeonCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellID) as UITableViewCell
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return neons.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("SELECTED")
+    }
 }
-
