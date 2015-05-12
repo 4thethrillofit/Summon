dc50c5c13e9c9e5acba25ac6cd4ef023c673b833
Feifan Wang <feifanw@gmail.com>
2015-05-12 00:44:14 -0700

    make the Neon object NSCoder compliant

--- Summon/Neon.swift
+++ Summon/Neon.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
-struct Neon {
-    let name: String
-    let description: String
-    let slackHandle: String
-    let specialty: String
-    var imageData: UIImage
+@objc(Neon)
+
+class Neon: NSObject, NSCoding {
+    var name: String = ""
+    var desc: String = ""
+    var slackHandle: String = ""
+    var specialty: String = ""
+    var imageData: UIImage = UIImage()
+    
+    init(name: String, desc: String, slackHandle: String, specialty: String, imageData: UIImage) {
+        self.name = name
+        self.desc = desc
+        self.slackHandle = slackHandle
+        self.specialty = specialty
+        self.imageData = imageData
+    }
+    
+    required init(coder unarchiver: NSCoder) {
+        super.init()
+        name = unarchiver.decodeObjectForKey("name") as! String
+        desc = unarchiver.decodeObjectForKey("desc") as! String
+        slackHandle = unarchiver.decodeObjectForKey("slackHandle") as! String
+        specialty = unarchiver.decodeObjectForKey("specialty") as! String
+        imageData = unarchiver.decodeObjectForKey("imageData") as! UIImage
+    }
+    
+    func encodeWithCoder(archiver: NSCoder) {
+        archiver.encodeObject(name, forKey: "name")
+        archiver.encodeObject(desc, forKey: "desc")
+        archiver.encodeObject(slackHandle, forKey: "slackHandle")
+        archiver.encodeObject(specialty, forKey: "specialty")
+        archiver.encodeObject(imageData, forKey: "imageData")
+    }
 }
